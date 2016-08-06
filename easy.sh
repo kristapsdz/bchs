197026da7ef44d9eff46a42975413e9ffd3484f6
kristapsdz <kristaps@bsd.lv>
2016-08-06 18:17:54 +0000

    Typo noted by Thiago Farina -- thanks!

--- easy.sh
+++ easy.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cc -static -g -W -Wall -o cgi cgi.c
-doas install -u www -g www -m 0500 cgi /var/www/cgi-bin
+doas install -o www -g www -m 0500 cgi /var/www/cgi-bin
 doas rcctl enable slowcgi
 doas rcctl start slowcgi
 doas rcctl check slowcgi
