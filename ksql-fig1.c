a3c8edfcc9ead44ea93d009d2cff0082ffb9fa23
kristapsdz <kristaps@bsd.lv>
2017-06-28 16:23:42 +0000

    Add ksql article.

---
+++ ksql-fig1.c
@@ -0,0 +1,6 @@
+/*
+ * Required to create database temporary files, read and write from
+ * them, and perform file-system operations (flock and fattr).
+ */
+if (pledge("stdio rpath cpath wpath flock fattr", NULL) == -1)
+	err(EXIT_FAILURE, "pledge");
