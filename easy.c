b84f5e0baba1e672a3ef677271a4202e903e75cd
kristapsdz <kristaps@bsd.lv>
2019-07-07 13:00:02 +0000

    style(9) and extra cc flags.

--- easy.c
+++ easy.c
@@ -1,16 +1,16 @@
-#include <err.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <err.h> /* err(3) */
+#include <stdlib.h> /* EXIT_xxxx */
+#include <stdio.h> /* puts(3) */
+#include <unistd.h> /* pledge(2) */
 
 int
 main(void)
 {
-    if (-1 == pledge("stdio", NULL)) 
+    if (pledge("stdio", NULL) == -1) 
         err(EXIT_FAILURE, "pledge");
     puts("Status: 200 OK\r");
     puts("Content-Type: text/html\r");
     puts("\r");
     puts("Hello, world!\n");
-    return(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
--- easy.sh
+++ easy.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-cc -static -g -W -Wall -o cgi cgi.c
+cc -static -g -W -Wall -Wextra -o cgi cgi.c
 doas install -o www -g www -m 0500 cgi /var/www/cgi-bin
 doas rcctl enable slowcgi
 doas rcctl start slowcgi
